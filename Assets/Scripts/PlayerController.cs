559f4c4f4818d6720635e334720d4b38b85c4a6f
Alex1274611 <153929775+Alex1274611@users.noreply.github.com>
2024-02-03 18:57:36 +0300

    D4V

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6;
+    public float jump = 10;
+    private float moveInputX; 
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        moveInputX = Input.GetAxis("Horizontal") * speed;
+        rb.velocity = new Vector2(moveInputX, rb.velocity.y);
+    }
+}
