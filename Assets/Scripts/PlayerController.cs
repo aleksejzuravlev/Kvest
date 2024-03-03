07c15689041bc4f75ce64d7750c2e73fb27e4c5a
Alex1274611 <153929775+Alex1274611@users.noreply.github.com>
2024-03-03 19:39:26 +0300

    D4R

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,49 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 6;
     public float jump = 10;
-    private float moveInputX; 
+    private float moveInputX;
+
+    public Transform startJumpCheckPoint;
+    public Transform endJumpCheckPoint;
 
     private Rigidbody2D rb;
+    private Animator animator;
+    private bool isGrounded = true;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     void Update()
     {
         moveInputX = Input.GetAxis("Horizontal") * speed;
         rb.velocity = new Vector2(moveInputX, rb.velocity.y);
+
+        //анимация бега
+        animator.SetFloat("Speed", Mathf.Abs(moveInputX));
+
+        Vector3 scale = transform.localScale;
+
+        if(moveInputX < 0)
+        {
+            scale.x = -Mathf.Abs(scale.x);
+        }
+        else if (moveInputX > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
+
+        isGrounded = Physics2D.Linecast(startJumpCheckPoint.position, endJumpCheckPoint.position);
+        print(isGrounded);
+        animator.SetBool("isGrounded", isGrounded);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rb.AddForce(transform.up * jump, ForceMode2D.Impulse);
+        }
     }
+
 }
