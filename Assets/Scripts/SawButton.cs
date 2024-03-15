330eaeebdef34415e95474b572e9e5f79b997960
Alex1274611 <153929775+Alex1274611@users.noreply.github.com>
2024-03-15 20:01:35 +0300

    last commit

---
+++ Assets/Scripts/SawButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawButton : MonoBehaviour
+{
+
+    public GameObject Saw;
+    public Sprite pushed;
+
+    private SpriteRenderer sr;
+    private bool stop = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && stop == false)
+        {
+            sr.sprite = pushed;
+
+            Animator sawAnimator = Saw.GetComponent<Animator>();
+            sawAnimator.SetTrigger("Stop");
+
+            Collider2D col = Saw.GetComponent<Collider2D>();
+            col.enabled = false;
+        }
+
+    }
+}
